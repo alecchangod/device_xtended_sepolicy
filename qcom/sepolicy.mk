d4d45624aee8e5b1b6429d7e4fdf2be4f63fdc1c
Michael Bestas <mkbestas@lineageos.org>
2021-10-07 22:04:41 +0300

    sepolicy: Switch to SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
    Fixes:
    warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead.
    warning: BOARD_PLAT_PUBLIC_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS instead.
    Change-Id: I752602079de8ff4c5370fe3ec861b8746838d878

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -9,11 +9,11 @@ TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
 endif
 endif
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/xtended/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/xtended/sepolicy/qcom/dynamic \
     device/xtended/sepolicy/qcom/system
 else
